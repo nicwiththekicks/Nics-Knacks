92ad199c28eaca9105ec7ecb2b96da12a8af9e70
nicwiththekicks <70962102+nicwiththekicks@users.noreply.github.com>
2020-11-30 12:47:09 -0500

    Update FractionTest.java

--- FractionTest.java
+++ FractionTest.java
@@ -2,7 +2,7 @@ import java.lang.Math ;
 public class FractionTest {
 	public static void main(String[] args) {
 	
-		Fraction frac1 = new Fraction(1,4);
+        Fraction frac1 = new Fraction(1,4);
         Fraction frac2 = new Fraction(2,3);
 	
         Fraction.addFraction(frac1,frac2); 
